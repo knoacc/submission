16fb4e39b4c5ff5232f07c8076b1747f49cd328b
knoacc <knoacc@users.noreply.github.com>
2020-09-16 04:02:40 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,19 +6,19 @@ remove elemen (hilangkan logo header)
 
 $(document).ready(function() {
     $(window).scroll(function() {
-        if ($(document).scrollTop() > 95) {
+        if ($(document).scrollTop() > 75) {
             $("#identity").hide();
         }
         else {
             $("#identity").show();
         };
-        if ($(document).scrollTop() > 90) {
+        if ($(document).scrollTop() > 70) {
             $("#nav").addClass("sticky");
         }
         else {
             $("#nav").removeClass("sticky");
         };
-        if ($(document).scrollTop() > 95) {
+        if ($(document).scrollTop() > 75) {
             $("#spasi").addClass("spasi");
         }
         else {
